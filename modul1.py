33f5172e9ed24c9457c29dfadd406eeef9c89025
Alina <elenna_ali@yahoo.com>
2021-04-14 21:27:21 +0300

    add module 1

---
+++ modul1.py
@@ -0,0 +1,34 @@
+print('Hello World')
+print('print\u200b("")')
+# print​("")
+HELLO_WORLD = 'Hello World'
+print(HELLO_WORLD)
+
+#name = 'Alina'
+#print(3 * (name + ' '))
+#name ='Alina'
+#print(name,name,name, sep='#' , end='')
+
+name='Alina'
+print(100* (name+'\n '))
+
+print(80*'#')
+print(3*name)
+print(name*3)
+print(print)
+
+
+# print int
+print(3)
+print('3')
+print('Triunghi: ')
+print("   / \ ")
+print("  /   \ ")
+print(' /     \ ')
+print("/_______\ ")
+
+print('Patrat: ')
+print(" _____")
+print("|     |")
+print("|_____|")
+
